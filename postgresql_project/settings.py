420ae3a033c69c92e2a159e8976d034ef2237236
chrstphrbls <109645414+chrstphrbls@users.noreply.github.com>
2022-07-27 15:25:28 +0800

    settings modified

--- postgresql_project/settings.py
+++ postgresql_project/settings.py
@@ -76,7 +76,11 @@ WSGI_APPLICATION = 'postgresql_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
 
